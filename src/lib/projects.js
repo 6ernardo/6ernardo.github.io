5329a6a3c48d008aab70b21a40435f036d6d4dbe
Bernardo <6ernardocampos@gmail.com>
2024-12-02 19:57:42 +0000

    Added LCOM proj

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -3,7 +3,7 @@ export const projects = [
     id: "gameoflife",
     name: "Game of Life",
     date: new Date("2024-09-02"),
-    description: "Implementation of Conway's Game of Life",
+    description: "Implementation of Conway's Game of Life.",
     image: "projects/gameoflife.png",
     repo: "https://github.com/6ernardo/gameoflife",
     site: "https://6ernardo.github.io/gameoflife/",
@@ -14,7 +14,7 @@ export const projects = [
     name: "Flying Bee",
     date: new Date("2024-05-21"),
     description:
-      "Final Project of the Computer Graphics unit",
+      "Final Project of the Computer Graphics unit.",
     image: "projects/flyingbee.png",
     repo: "https://github.com/6ernardo/feup-CGRA-proj",
     site: "https://6ernardo.github.io/feup-CGRA-proj/",
@@ -25,7 +25,7 @@ export const projects = [
     name: "UP Connect",
     date: new Date("2023-12-21"),
     description:
-      "Project of the LBAW Unit. Q&A Platform similar to Reddit",
+      "Project of the LBAW Unit. Q&A Platform similar to Reddit.",
     repo: "https://github.com/6ernardo/feup-LBAW",
     tags: ['html', 'css', 'laravel', 'postgresql', 'javascript']
   },
@@ -67,5 +67,15 @@ export const projects = [
     repo: "https://github.com/6ernardo/6ernardo.github.io",
     site: "https://6ernardo.github.io/",
     tags: ['svelte' ,'javascript', 'tailwindcss']
+  },
+  {
+    id: "feup-LCOM-proj",
+    name: "VS Draw",
+    date: new Date("2023-06-17"),
+    description:
+      "Project for the LCOM unit. Drawing tool using device drivers built from scratch.",
+    image: "projects/vsdraw.png",
+    repo: "https://github.com/6ernardo/feup-LCOM/tree/master/proj",
+    tags: ['c' , 'drivers']
   },
 ];
