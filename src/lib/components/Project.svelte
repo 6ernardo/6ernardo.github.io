d1f89e699f15bf05c89c7900e1d0fd385f106332
Bernardo <6ernardocampos@gmail.com>
2025-02-01 19:03:15 +0000

    Projects' title and image now clickable. Replaced larger images with modern formats for performance

--- src/lib/components/Project.svelte
+++ src/lib/components/Project.svelte
@@ -1,42 +1,64 @@
 <script>
-  export let project;
+	export let project;
 </script>
 
 <article
-  class="relative flex flex-col overflow-hidden rounded bg-slate-800 p-2 md:p-4"
+	class="relative flex flex-col overflow-hidden rounded bg-slate-800 p-2 md:p-4"
 >
-  <h3
-    class="text-2xl font-semibold {project.image
-      ? 'absolute top-0 left-0 p-2 bg-slate-800 rounded-br shadow-lg'
-      : ''}"
-  >
-    {project.name}
-  </h3>
-  {#if project.image}
-    <img
-      class="-m-2 mb-2 aspect-video h-auto w-[calc(theme(spacing.4)+100%)] max-w-[unset] object-cover md:-m-4 md:mb-2 md:w-[calc(theme(spacing.8)+100%)]"
-      src={project.image}
-      alt=""
-    />
-  {/if}
-  <p class="{project.tags ? '' : 'mb-auto'} mt-1">{project.description}</p>
+	{#if project.image}
+		<a
+			href={project.repo}
+			target="_blank"
+			rel="noopener noreferrer"
+			class="block relative"
+		>
+			<img
+				class="-m-2 mb-2 aspect-video h-auto w-[calc(theme(spacing.4)+100%)] max-w-[unset] object-cover md:-m-4 md:mb-2 md:w-[calc(theme(spacing.8)+100%)]"
+				src={project.image}
+				alt=""
+			/>
+		</a>
+	{/if}
 
-  {#if project.tags}
-    <div class="flex flex-wrap mt-0.5 mb-auto">
-      {#each project.tags as tag}
-        <div class="mr-2 mt-1 text-sm bg-slate-900 p-1.5 rounded text-slate-300" >{tag}</div>
-      {/each}
-    </div>
-  {/if}
-  
-  <nav class="flex">
-    <a href={project.repo} target="_blank" rel="noopener noreferrer"
-      ><img class="size-7 m-1 mt-2 mr-3" src="icons/github.svg" alt="" /></a
-    >
-    {#if project.site}
-      <a href={project.site} target="_blank" rel="noopener noreferrer"
-        ><img class="size-7 m-1 mt-2" src="icons/site.svg" alt="Site" /></a
-      >
-    {/if}
-  </nav>
+	<a
+		href={project.repo}
+		target="_blank"
+		rel="noopener noreferrer"
+		class="absolute top-0 left-0 p-2 bg-slate-800 rounded-br shadow-lg"
+	>
+		<h3 class="text-2xl font-semibold">{project.name}</h3>
+	</a>
+
+	<p class="{project.tags ? '' : 'mb-auto'} {project.image ? 'mt-1' : 'mt-10'}">{project.description}</p>
+
+	{#if project.tags}
+		<div class="flex flex-wrap mt-0.5 mb-auto">
+			{#each project.tags as tag}
+				<div
+					class="mr-2 mt-1 text-sm bg-slate-900 p-1.5 rounded text-slate-300"
+				>
+					{tag}
+				</div>
+			{/each}
+		</div>
+	{/if}
+
+	<nav class="flex">
+		<a href={project.repo} target="_blank" rel="noopener noreferrer"
+			><img
+				class="size-7 m-1 mt-2 mr-3"
+				src="icons/github.svg"
+				alt=""
+			/></a
+		>
+		{#if project.site}
+			<a href={project.site} target="_blank" rel="noopener noreferrer"
+				><img
+					class="size-7 m-1 mt-2"
+					src="icons/site.svg"
+					alt="Site"
+				/></a
+			>
+		{/if}
+	</nav>
 </article>
--- src/lib/projects.js
+++ src/lib/projects.js
@@ -1,94 +1,94 @@
 export const projects = [
-  {
-    id: "gameoflife",
-    name: "Game of Life",
-    date: new Date("2024-09-02"),
-    description: "Implementation of Conway's Game of Life.",
-    image: "projects/gameoflife.png",
-    repo: "https://github.com/6ernardo/gameoflife",
-    site: "https://6ernardo.github.io/gameoflife/",
-    tags: ['p5.js', 'javascript']
-  },
-  {
-    id: "feup-cgra-proj",
-    name: "Flying Bee",
-    date: new Date("2024-05-21"),
-    description:
-      "Final Project of the Computer Graphics unit.",
-    image: "projects/flyingbee.png",
-    repo: "https://github.com/6ernardo/feup-CGRA-proj",
-    site: "https://6ernardo.github.io/feup-CGRA-proj/",
-    tags: ['javascript', 'webgl']
-  },
-  {
-    id: "feup-lbaw",
-    name: "UP Connect",
-    date: new Date("2023-12-21"),
-    description:
-      "Project of the LBAW Unit. Q&A Platform similar to Reddit.",
-    image: "projects/upconnect.png",
-    repo: "https://github.com/6ernardo/feup-LBAW",
-    tags: ['html', 'css', 'laravel', 'postgresql', 'javascript']
-  },
-  {
-    id: "feup-comp",
-    name: "Java-- compiler",
-    date: new Date("2024-05-24"),
-    description:
-      "Project for the Compiler Unit. Built a compiler for a subset of Java.",
-    repo: "https://github.com/6ernardo/feup-COMP",
-    tags: ['java']
-  },
-  {
-    id: "feup-LTW-proj",
-    name: "Ticket Website",
-    date: new Date("2023-06-30"),
-    description:
-      "Project for the LTW unit. Plataform for support ticket management.",
-    image: "projects/ticketwebsite.png",
-    repo: "https://github.com/6ernardo/feup-LTW-proj",
-    tags: ['php', 'html', 'css', 'sqlite', 'javascript']
-  },
-  {
-    id: "feup-LDTS-proj",
-    name: "Thin Ice",
-    date: new Date("2023-06-17"),
-    description:
-      "Project for the LDTS unit. Club Penguin's Thin Ice minigame clone.",
-    image: "projects/thinice.png",
-    repo: "https://github.com/6ernardo/feup-LDTS/tree/master/proj",
-    tags: ['java', 'lanterna', 'oop']
-  },
-  {
-    id: "portfolio",
-    name: "Portoflio Website",
-    date: new Date("2024-10-07"),
-    description:
-      "Website you are currently on. Personal website/portfolio.",
-    image: "projects/portfolio.png",
-    repo: "https://github.com/6ernardo/6ernardo.github.io",
-    site: "https://6ernardo.github.io/",
-    tags: ['svelte' ,'javascript', 'tailwindcss']
-  },
-  {
-    id: "feup-LCOM-proj",
-    name: "VS Draw",
-    date: new Date("2023-06-17"),
-    description:
-      "Project for the LCOM unit. Drawing tool using device drivers built from scratch.",
-    image: "projects/vsdraw.png",
-    repo: "https://github.com/6ernardo/feup-LCOM/tree/master/proj",
-    tags: ['c' , 'drivers', 'minix']
-  },
-  {
-    id: "sandsimulator",
-    name: "Sand Simulator",
-    date: new Date("2024-12-11"),
-    description:
-      "Simple particle simulator.",
-    image: "projects/sandsimulator.png",
-    repo: "https://github.com/6ernardo/sandsimulator",
-    site: "https://6ernardo.github.io/sandsimulator/",
-    tags: ['p5.js' , 'javascript']
-  },
+	{
+		id: "gameoflife",
+		name: "Game of Life",
+		date: new Date("2024-09-02"),
+		description: "Implementation of Conway's Game of Life.",
+		image: "projects/gameoflife.png",
+		repo: "https://github.com/6ernardo/gameoflife",
+		site: "https://6ernardo.github.io/gameoflife/",
+		tags: ['p5.js', 'javascript']
+	},
+	{
+		id: "feup-cgra-proj",
+		name: "Flying Bee",
+		date: new Date("2024-05-21"),
+		description:
+			"Final Project of the Computer Graphics unit.",
+		image: "projects/flyingbee.webp",
+		repo: "https://github.com/6ernardo/feup-CGRA-proj",
+		site: "https://6ernardo.github.io/feup-CGRA-proj/",
+		tags: ['javascript', 'webgl']
+	},
+	{
+		id: "feup-lbaw",
+		name: "UP Connect",
+		date: new Date("2023-12-21"),
+		description:
+			"Project of the LBAW Unit. Q&A Platform similar to Reddit.",
+		image: "projects/upconnect.webp",
+		repo: "https://github.com/6ernardo/feup-LBAW",
+		tags: ['html', 'css', 'laravel', 'postgresql', 'javascript']
+	},
+	{
+		id: "feup-comp",
+		name: "Java-- compiler",
+		date: new Date("2024-05-24"),
+		description:
+			"Project for the Compiler Unit. Built a compiler for a subset of Java.",
+		repo: "https://github.com/6ernardo/feup-COMP",
+		tags: ['java']
+	},
+	{
+		id: "feup-LTW-proj",
+		name: "Ticket Website",
+		date: new Date("2023-06-30"),
+		description:
+			"Project for the LTW unit. Plataform for support ticket management.",
+		image: "projects/ticketwebsite.png",
+		repo: "https://github.com/6ernardo/feup-LTW-proj",
+		tags: ['php', 'html', 'css', 'sqlite', 'javascript']
+	},
+	{
+		id: "feup-LDTS-proj",
+		name: "Thin Ice",
+		date: new Date("2023-06-17"),
+		description:
+			"Project for the LDTS unit. Club Penguin's Thin Ice minigame clone.",
+		image: "projects/thinice.png",
+		repo: "https://github.com/6ernardo/feup-LDTS/tree/master/proj",
+		tags: ['java', 'lanterna', 'oop']
+	},
+	{
+		id: "portfolio",
+		name: "Portoflio Website",
+		date: new Date("2024-10-07"),
+		description:
+			"Website you are currently on. Personal website/portfolio.",
+		image: "projects/portfolio.png",
+		repo: "https://github.com/6ernardo/6ernardo.github.io",
+		site: "https://6ernardo.github.io/",
+		tags: ['svelte', 'javascript', 'tailwindcss']
+	},
+	{
+		id: "feup-LCOM-proj",
+		name: "VS Draw",
+		date: new Date("2023-06-17"),
+		description:
+			"Project for the LCOM unit. Drawing tool using device drivers built from scratch.",
+		image: "projects/vsdraw.webp",
+		repo: "https://github.com/6ernardo/feup-LCOM/tree/master/proj",
+		tags: ['c', 'drivers', 'minix']
+	},
+	{
+		id: "sandsimulator",
+		name: "Sand Simulator",
+		date: new Date("2024-12-11"),
+		description:
+			"Simple particle simulator.",
+		image: "projects/sandsimulator.png",
+		repo: "https://github.com/6ernardo/sandsimulator",
+		site: "https://6ernardo.github.io/sandsimulator/",
+		tags: ['p5.js', 'javascript']
+	},
 ];
